8e01ec5d13f26dbc501f47efce7622246bef80d9
dmorcos6174 <davidmorcos3720@gmail.com>
2023-11-17 20:14:35 +0200

    tested rest template and web client

--- apigateway/src/main/java/apigateway/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/apigateway/apigateway/ApigatewayApplication.java
@@ -1,29 +1,12 @@
 package apigateway.apigateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 @SpringBootApplication
 public class ApigatewayApplication {
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-
-	@Bean
-	@LoadBalanced
-	public WebClient.Builder getWebClientBuilder() {
-		return WebClient.builder();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
-
 }
